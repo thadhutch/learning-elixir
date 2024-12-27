bb43b04e685fc04d2ffd91547fea1beed9f79fd2
Thad Hutcheson <thadhutch@gmail.com>
2024-12-26 18:33:42 -0600

    language list

---
+++ elixir/language-list/lib/language_list.ex
@@ -0,0 +1,24 @@
+defmodule LanguageList do
+  def new() do
+    empty_list = []
+  end
+  def add(list, language) do
+    [ language | list ]
+  end
+
+  def remove(list) do
+    list = tl(list)
+  end
+
+  def first(list) do
+    List.first(list)
+  end
+
+  def count(list) do
+    length(list)
+  end
+
+  def functional_list?(list) do
+    "Elixir" in list
+  end
+end
